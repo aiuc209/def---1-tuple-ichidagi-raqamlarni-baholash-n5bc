26be36153756e32cdd4ce364705561f6e646aa79
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:29:06 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Def   1 Tuple Ichidagi Raqamlarni Baholash N5bc
+Topic: DEF - 1: ��Tuple ichidagi raqamlarni baholash
+"""
+
+def main():
+    print("Welcome to Def   1 Tuple Ichidagi Raqamlarni Baholash N5bc!")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
